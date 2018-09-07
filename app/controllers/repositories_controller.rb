7378c58168b068cc7a09ba35873303df12177982
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-09-07 12:25:09 -0400

    browser pry and test pry act differently, who knew?

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,27 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    @resp = Faraday.get 'https://api.github.com/user/repos', {}, {
+      'Authorization' => "token #{session[:token]}",
+      'Accept' => 'application/json'
+    }
+    body = JSON.parse(@resp.body)
+    if @resp.success?
+      @repositories = body
+      # @username = @repositories[1]["owner"]["login"]
+    else
+      @error = body["meta"]["errorDetail"]
+    end
+  end
+
+  def create
+    @resp = Faraday.post 'https://api.github.com/user/repos', {
+      name: params[:name]
+    }.to_json, {
+      'Authorization' => "token #{session[:token]}",
+      'Accept' => 'application/json'
+    }
+    redirect_to root_path
   end
 
 end
