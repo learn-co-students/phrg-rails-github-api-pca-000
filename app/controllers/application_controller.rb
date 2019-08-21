ea483b469084766ec957c9b34a41ed95d9dcbdee
Gillian Wenhold <gwenhold@mac.com>
2019-08-21 12:59:13 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,16 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user
 
-  private
+private
 
-    def authenticate_user
-      # make sure to pass in the scope parameter (`repo` scope should be appropriate for what we want to do) in step of the auth process!
-      # https://developer.github.com/apps/building-oauth-apps/authorization-options-for-oauth-apps/#web-application-flow
-    end
+  def authenticate_user
+    client_id = ENV['GITHUB_CLIENT_ID']
+    redirect_uri = CGI.escape("http://localhost:3000/auth")
+    github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}"
+    redirect_to github_url unless logged_in?
+  end
 
-    def logged_in?
-    end
+  def logged_in?
+    !!session[:token]
+  end
 end
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,8 @@
 class RepositoriesController < ApplicationController
-  
   def index
+    response = Faraday.get "https://api.github.com/user", {}, {'Authorization': "token #{session[:token]}", 'Accept': 'application/json'}
+    second_response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization': "token #{session[:token]}", 'Accept': 'application/json'}
+    @name = JSON.parse(response.body)
+    @repos = JSON.parse(second_response.body)
   end
-
 end
